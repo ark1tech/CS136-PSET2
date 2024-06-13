5d654718e6a341a4238ce8914c43aeb1e224e1ad
RK Mañago <rbmanago1@up.edu.ph>
2024-06-13 08:25:57 +0800

    feat: finish item 1

--- test.py
+++ test.py
@@ -6,5 +6,5 @@ n_points = 9
 
 
 for x in range(1, n_points+1):
-    print(start+(end-start)*((-math.cos(((x-1)*math.pi)/n_points)+1)/2), x)
+    print(start+(end-start)*((-math.cos(((x-1)*math.pi)/(n_points-1))+1)/2), x)
     # print((start + end)/2 + ((end-start)/2)*(math.cos(((2*x+1)/(2*n_points))*math.pi)), x)
