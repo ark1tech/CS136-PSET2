a1c5b8c787b5956c368ca0eb8e487d46b4371ff7
Jose <joseernestotmanan@gmail.com>
2024-06-21 05:42:52 +0800

    feat: refactored to reflect ipynb

--- SUBMITTABLE 2.py
+++ SUBMITTABLE 2.py
@@ -5,13 +5,31 @@ import plotly.graph_objects as go
 # Gaussian quadrature constants 
 # Notation: (w_i, x_i)
 GAUSS_LAGUERRE = [
-    (0.458964, 0.222847), (0.417, 1.188932), (0.113373, 2.992736),
-    (0.0103992, 5.775144),(0.000261017, 9.837467), (0.000000898548, 15.982874)
+    (0.458964, 0.222847), (0.417, 1.188932), (0.113373, 2.992736), (0.0103992, 5.775144),
+    (0.000261017, 9.837467), (0.000000898548, 15.982874)
 ]
 
 GAUSS_LEGENDRE = [
-    (0.360762, -0.661209), (0.467914, -0.238619), (0.171324, -0.932469),
-    (0.171324, 0.932469), (0.467914, 0.238619), (0.360762, 0.661209)
+    (0.0775059479784248, -0.0387724175060508), (0.0775059479784248, 0.0387724175060508),
+    (0.077039818164248, -0.1160840706752552), (0.077039818164248, 0.1160840706752552),
+    (0.0761103619006262, -0.1926975807013711), (0.0761103619006262, 0.1926975807013711),
+    (0.0747231690579683, -0.2681521850072537), (0.0747231690579683, 0.2681521850072537),
+    (0.0728865823958041, -0.3419940908257585), (0.0728865823958041, 0.3419940908257585),
+    (0.0706116473912868, -0.413779204371605), (0.0706116473912868, 0.413779204371605),
+    (0.0679120458152339, -0.4830758016861787), (0.0679120458152339, 0.4830758016861787),
+    (0.064804013456601, -0.5494671250951282), (0.064804013456601, 0.5494671250951282),
+    (0.0613062424929289, -0.6125538896679802), (0.0613062424929289, 0.6125538896679802),
+    (0.0574397690993916, -0.6719566846141796), (0.0574397690993916, 0.6719566846141796),
+    (0.0532278469839368, -0.7273182551899271), (0.0532278469839368, 0.7273182551899271),
+    (0.0486958076350722, -0.7783056514265194), (0.0486958076350722, 0.7783056514265194),
+    (0.0438709081856733, -0.8246122308333117), (0.0438709081856733, 0.8246122308333117),
+    (0.038782167974472, -0.8659595032122595), (0.038782167974472, 0.8659595032122595),
+    (0.0334601952825478, -0.9020988069688743), (0.0334601952825478, 0.9020988069688743),
+    (0.0279370069800234, -0.9328128082786765), (0.0279370069800234, 0.9328128082786765),
+    (0.022245849194167, -0.9579168192137917), (0.022245849194167, 0.9579168192137917),
+    (0.0164210583819079, -0.9772599499837743), (0.0164210583819079, 0.9772599499837743),
+    (0.0104982845311528, -0.990726238699457), (0.0104982845311528, 0.990726238699457),
+    (0.0045212770985332, -0.9982377097105593), (0.0045212770985332, 0.9982377097105593)
 ]
 
 # Weibull distribution function -- uses Gauss-Laguerre quadratures
@@ -34,8 +52,8 @@ def cdf (t, k, mu):
 
     return pdf_area
 
-# Inverse survivability -- solved as root-finding problem using bisection method
-def bisection (a, b, alpha, mu, k):
+# Inverse survivability -- solved as root-finding problem using regula falsi method
+def regula_falsi(a, b, alpha, mu, k):
     f = lambda t : 1 - cdf(t, k, mu) - alpha
 
     tol = 1.0e-9
@@ -49,22 +67,16 @@ def bisection (a, b, alpha, mu, k):
     if np.sign(fa) == np.sign(fb):
         return None
     
-    n = int (math.ceil (math.log(abs(b-a)/tol) / math.log(2.0)))
-
-    for i in range(n):
-        c = 0.5 * (a + b)
+    while True:
+        c = b - fb * (b-a)/(fb-fa)
         fc = f(c)
 
-        if fc == 0.0:
+        if fc == 0.0 or abs(fc) < tol:
             return c
-        if np.sign(fa) != np.sign(fc):
+        elif fa*fc < 0:
             b = c
-            fb = fc
-        elif np.sign(fb) != np.sign(fc):
-            a = c
-            fa = fc
-
-    return 0.5 * (a+b)
+        else:
+            a = c 
 
 # Plotting function
 def plot (x05, x1, x2, y05, y1, y2, xtitle, ytitle, title):
@@ -76,10 +88,38 @@ def plot (x05, x1, x2, y05, y1, y2, xtitle, ytitle, title):
     ])
     fig.update_layout(
         title_text=title,
+        xaxis_title=xtitle,
+        yaxis_title=ytitle,
         height=1080*0.5,
         width=1920*0.6,
+        font_family="CMU Serif",
+        font_size=15,
+        title_font_size=25,
+        font_color="#0e0f11",
+        margin=dict(t=120, b=80)
+    )
+    fig.show()
+
+# Plotting function: inverse
+def plot_inverse (x05, x1, x2, y05, y1, y2, xtitle, ytitle, title):
+    fig = go.Figure()
+    fig.add_traces([
+        go.Scatter(x=x05, y=y05, mode='lines', marker = {'color' : 'blue'}, name="k = 0.5"),
+        go.Scatter(x=x1, y=y1, mode='lines', marker = {'color' : 'red'}, name="k = 1"),
+        go.Scatter(x=x2, y=y2, mode='lines', marker = {'color' : 'magenta'}, name="k = 2"),
+        go.Scatter(x=[i/100 for i in range(0, 101, 1)], y=[78 for i in range(0, 101, 1)], line_dash ='dash', marker = {'color' : 'orange'}, name="Life expectancy"),
+    ])
+    fig.update_layout(
+        title_text=title,
         xaxis_title=xtitle,
-        yaxis_title=ytitle
+        yaxis_title=ytitle,
+        height=1080*0.5,
+        width=1920*0.6,
+        font_family="CMU Serif",
+        font_size=15,
+        title_font_size=25,
+        font_color="#0e0f11",
+        margin=dict(t=120, b=80)
     )
     fig.show()
 
@@ -93,9 +133,9 @@ mu = 78
 for alpha in range(1, 100, 1):
     alpha /= 100
     x_values.append(alpha)
-    y_t_05.append(bisection(1, 250, alpha, mu, 0.5))
-    y_t_1.append(bisection(1, 250, alpha, mu, 1))
-    y_t_2.append(bisection(1, 250, alpha, mu, 2))
+    y_t_05.append(regula_falsi(1, 250, alpha, mu, 0.5))
+    y_t_1.append(regula_falsi(1, 250, alpha, mu, 1))
+    y_t_2.append(regula_falsi(1, 250, alpha, mu, 2))
 
 # Plotting proper
 plot(x_values, x_values, x_values, y_t_05, y_t_1, y_t_2, "Probability (alpha)", "Time (t)", "Inverse survival (mu = 78)")
